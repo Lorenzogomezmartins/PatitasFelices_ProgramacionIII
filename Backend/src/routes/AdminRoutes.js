e98c2572aeaae25ff65a9d86a4970dbd4330cd9d
Santino fernandez <santinojonasfernandez14@gmail.com>
2025-11-16 20:35:07 -0300

    style: ultimos detalles del modo oscuro

--- Backend/src/routes/AdminRoutes.js
+++ Backend/src/routes/AdminRoutes.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const authAdmin = require('../middlewares/authAdmin');
-
 const CrearAdmin = require('../controllers/admin/CrearAdmin');
 const ObtenerAdmins = require('../controllers/admin/ObtenerAdmin');
 const ObtenerAdminPorId = require('../controllers/admin/ObtenerAdminPorId');
@@ -12,13 +10,10 @@ const LoginAdmin = require('../controllers/admin/LoginAdmin');
 
 // LOGIN 
 router.post('/login', LoginAdmin);
-
-// TODO LO DEMÁS → REQUIERE TOKEN
-router.use(authAdmin);
-
+// RUTAS PROTEGIDAS
 router.get('/', ObtenerAdmins);
 router.get('/:id', ObtenerAdminPorId);
-router.post('/', CrearAdmin);
+router.post('/' , CrearAdmin);
 router.put('/:id', ActualizarAdmin);
 router.delete('/:id', EliminarAdmin);
 
